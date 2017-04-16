ec798e900fe93121da0cf51619bda774dba12089
Alan Fidelino <afidedev26@gmail.com>
2017-04-16 17:18:01 +1000

    Add firebase scripts to gitignore

---
+++ public/js/firebase-secure.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBuzPXNWXOuQUXhQ3npPPwoYLbGC48g0dg",
+    authDomain: "angregistration17.firebaseapp.com",
+    databaseURL: "https://angregistration17.firebaseio.com",
+    projectId: "angregistration17",
+    storageBucket: "angregistration17.appspot.com",
+    messagingSenderId: "1052391111526"
+  };
+  firebase.initializeApp(config);
