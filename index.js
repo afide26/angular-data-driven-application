64d343f5f7ff99447fd3ecbef4a255a6425c1680
Alan Fidelino <afidedev26@gmail.com>
2017-04-16 14:21:27 +1000

    Add scripts and styles

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/public/views'));
+app.use(express.static(__dirname + '/public'));
+
 app.listen(port, ()=>{
   console.log(`App running at port ${port}`);
 })
